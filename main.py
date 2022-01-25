2eee0b0d0b9f0565304ee62c122b36a27a33a461
Fizzy <tungdongviet3@gmail.com>
2022-01-25 22:26:29 +0700

    extend visualization

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import ChartModule
+from mesa.visualization.ModularVisualization import VisualizationElement
+import numpy as np
 
 from MoneyModel import MoneyModel
+
+class HistogramModule(VisualizationElement):
+    package_includes = ["Chart.min.js"]
+    local_includes = ["HistogramModule.js"]
+
+    def __init__(self, bins, canvas_height, canvas_width):
+        self.canvas_height = canvas_height
+        self.canvas_width = canvas_width
+        self.bins = bins
+        new_element = "new HistogramModule({}, {}, {})"
+        new_element = new_element.format(bins,
+                                         canvas_width,
+                                         canvas_height)
+        self.js_code = "elements.push(" + new_element + ");"
+
+    def render(self, model):
+        wealth_vals = [agent.wealth for agent in model.schedule.agents]
+        hist = np.histogram(wealth_vals, bins=self.bins)[0]
+        return [int(x) for x in hist]
 
 def agent_potrayal(agent):
   potrayal = {
@@ -36,12 +57,18 @@ chart = ChartModule([{
   "Label": "Gini",
   "Color": "Black"}],
   data_collector_name = "datacollector")
-
+"""
 server = ModularServer(
   MoneyModel,
   [grid, chart],
   "Money Model",
   {"N": 50, "width": 30, "height": 30}
   )
+"""
+histogram = HistogramModule(list(range(10)), 800, 800)
+server = ModularServer(MoneyModel,
+                       [grid, histogram, chart],
+                       "Money Model",
+                       {"N":100, "width":30, "height":30})
 server.port = 8521
 server.launch()
